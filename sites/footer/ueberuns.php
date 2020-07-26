ae988dd0ed24bba933d7530d980b86dd1d65483c
Tachyon1106 <60649430+Tachyon1106@users.noreply.github.com>
2020-07-26 16:36:41 +0200

    added ueberuns

--- sites/footer/ueberuns.php
+++ sites/footer/ueberuns.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="ueberuns.css">
+    <title>Wer sind wir?</title>
+    </head>
     <body>
-        <p>&Uumlber uns</p>
+        <div id="gesamt">
+            <h1>Wer sind wir?</h1>
+            <p> Wir sind vier Informatik-Studenten, die im Rahmen der Vorlesung "Web-Engineering" diesen Shop erstellt haben.</p>
+        </div>
     </body>
 </html>
