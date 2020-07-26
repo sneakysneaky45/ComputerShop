f47c01a0ccc0b214daeeb4c7c48c247139022b7a
Tachyon1106 <60649430+Tachyon1106@users.noreply.github.com>
2020-07-26 16:17:06 +0200

    ABG added

--- sites/footer/agbs.php
+++ sites/footer/agbs.php
@@ -1,6 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html lang="de">
+    <head>
+    <meta charset="utf-8">
+    <title>AGB</title>
+    <link rel="stylesheet" href="agbs.css">
+    </head>
+
     <body>
-        <p>AGBs</p>
+        <div id="gesamt">
+            <h2>Allgemeine Geschaeftsbedingungen</h2>
+
+            <h3>1. Allgemeines</h3>
+            <a>Die in diesem Shop verwendete Sprache ist Deutsch. Die verwendete Währung Euro (€).</a>
+
+            <h3>2. Datenschutz</h3>
+            <a>Unsere Datenschutz-Bestimmungen finden sie</a>
+            <a href="../footer/datenschutz.php">hier</a>
+            <a>.</a>
+
+            <h3>3. Kommunikation</h3>
+            <a>Sollten Sie auf Probleme stoßen, können sie diese über die E-Mail </a>
+            <a href="#">support@supportman.com</a>
+            <a>unserem Team mitteilen und Hilfe erhalten. Sollten sie Bugs o.ä. entdecken, können Sie diese an</a>
+            <a href="#">techsupport@techsupportman.com</a>
+            <a>senden, sodass diese behoben werden können. Bei Login-Problemen und Beschwerden wenden Sie sich bitte an</a>
+            <a href="#">moderator@moderatorman.com</a>
+            <a>oder in sehr dringenden Fällen an</a>
+            <a href="#">admin@adminman.com</a>
+            <a>. Vielen Dank.</a>
+
+            <h3>4. Versand</h3>
+            <a>Der Versand wird durch --- realisiert. Nach ihrer Bestellung behalten wir uns eine Versandzeit von 3 Werktagen ein (sofern nicht anders in Produktbeschreibung angegeben). Die Versandkosten trägt der Käufer. Diese werden während des Bezahlvorgangs angezeigt und eingerechnet.</a>
+            
+            <h3>5. Rückgaberecht</h3>
+            <a>Der Käufer besitzt Rückgaberecht von einem Produkt oder der gesamten Bestellung, falls:</a>
+            <p>- Teile des Produkts/der Bestellung fehlen.</p>
+            <a>- Teile des Produkts/der Bestellung während des Transports kaputtgegangen sind.</a>
+
+            <h3>6. Bezahlmöglichkeiten</h3>
+            <a>Bezahlmöglichkeiten sind: Klarna (Sofortüberweisung), Kreditkarte, PayPal.</a>
+
+            <h3>7. Änderungen der AGB</h3>
+            <a> Der Shop behält sich das Recht ein, die AGB jederzeit zu ändern. Diese Änderungen werden Mitgliedern per E-Mail mitgeteilt und müssen vom Vorstand beschlossen und bestätigt werden. Bestellungen, die vor der Änderung getätigt wurden, sind nicht von dieser betroffen. Es existiert eine Vorlaufzeit von 36 h von Änderung bis Inkraftreten der Änderung. </a>
+            <h3>Ungültigkeitsklausel</h3>
+            <a>Da dieser Shop niemals tatsächlich realisiert wird, sind diese gesamten AGB ungültig und nicht von Belang. Der Vollständigkeit halber und weil sie dazugehören sind sie trotzdem vorhanden und möglichst gut ausformuliert.</a>
+        </div>
     </body>
 </html>
