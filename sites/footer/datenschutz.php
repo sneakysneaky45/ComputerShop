c36f38f288297c5b53dac7361c0b48eac15f84ad
Tachyon1106 <60649430+Tachyon1106@users.noreply.github.com>
2020-07-26 20:15:13 +0200

    datenschutz impressum

--- sites/footer/datenschutz.php
+++ sites/footer/datenschutz.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Datenschutz</title>
+    <link rel="stylesheet" href="datenschutz.css">
+    </head>
     <body>
-        <p>Ihre Daten werden nicht geschützt nice!</p>
+        <div id="gesamt">
+            <h1>Datenschutzerkl&aumlrung</h1>
+            <a>Ihre Kontodaten werden End2End verschl&uumlsselt. Ihr Passwort kann nur vom Admin eingesehen und nur von Ihnen und dem Admin geändert werden. Um ihr Konto zu sperren zu lassen, wenden Sie sich bitte an</a>
+            <a href="#">moderator@moderatorman.com</a>
+            <a>.</a>
+        </div>
     </body>
 </html>
--- sites/footer/subaroimprezzo.php
+++ sites/footer/subaroimprezzo.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Impressum</title>
+    <link rel="stylesheet" href="subaroimprezzo.css">
+    </head>
     <body>
-        <p>Imprezzo goes brrrr</p>
+        <div id="gesamt">
+            <h1>Impressum</h1>
+            <a>Shop gemacht von: Frank, David ; Jahn, Niklas ; Klauber, Paul ; Luitle, Roman </a>
+            <p>Adresse: 69420 Datenschutzdorf, Sicherheitsstr. 1337</p>
+            <a>Kontakt: E-Mail siehe AGB, Telefon: 0100111001100101011010010110111000001010</a>
+         </div>
     </body>
 </html>
